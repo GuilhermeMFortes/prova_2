743d55645f1bdb77629d69f13402d819623e7128
GuilhermeMFortes <guilherme.mendes.fortes@gmail.com>
2022-11-28 22:05:16 -0300

    [finishes #183906244] update products.js

--- products.js
+++ products.js
@@ -24,26 +24,40 @@ const produtos = [
 ]
 
 
-function listar() {
-    produtos.forEach(p => {
-        console.log(p)
-    })
-}
+// function listar() {
+//     produtos.forEach(p => {
+//         console.log(p)
+//     })
+// }
 
-function prodEscolar (value){
-    if (value.categoria == "Escolar")
-    return value;
-}
-var produtosEscolar = produtos.filter(prodEscolar);
-produtosEscolar.forEach(produtoEscolar => { 
-    console.log(produtoEscolar);
-})
+// function prodEscolar (value){
+//     if (value.categoria == "Escolar")
+//     return value;
+// }
+// var produtosEscolar = produtos.filter(prodEscolar);
+// produtosEscolar.forEach(produtoEscolar => { 
+//     console.log(produtoEscolar);
+// })
 
-function prodEletronico (value){
-    if (value.categoria == "Eletrônico")
+// function prodEletronico (value){
+//     if (value.categoria == "Eletrônico")
+//     return value;
+// }
+// var produtosEletronico = produtos.filter(prodEletronico);
+// produtosEletronico.forEach(produtoEletro => { 
+//     console.log(produtoEletro);
+// })
+
+function valor (value){
+    let min_valor = 0
+    let max_valor = 2000
+    if (value.valor >= min_valor && value.valor <= max_valor) 
     return value;
+    else {
+        console.log("Existe não, amigo brother")
+    }
 }
-var produtosEletronico = produtos.filter(prodEletronico);
-produtosEletronico.forEach(produtoEletro => { 
-    console.log(produtoEletro);
+var valor = produtos.filter(valor);
+valor.forEach(valor => { 
+    console.log(valor);
 })
